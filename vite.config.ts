46943ceb3347608c7929c9ca234186735addf9f7
Panupong Tipjoi <itstoon.panupong@gmail.com>
2024-10-31 05:00:03 +0700

    feat: Setup tanstack router for vite and tsconfig paths for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		react(),
+		tsconfigPaths(),
+		TanStackRouterVite(),
 		VitePWA({
 			registerType: 'prompt',
 			injectRegister: false,
