bd04a1f58961b8553099ad6741b87114431d0c37
Panupong Tipjoi <itstoon.panupong@gmail.com>
2024-11-02 07:25:15 +0700

    feat: Added tanstack query eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,14 @@
-const eslintConfig = {
+module.exports = {
 	root: true,
 	env: { browser: true, es2020: true },
 	extends: [
 		'eslint:recommended',
-		'plugin:@typescript-eslint/strict-type-checked',
-		'plugin:react-hooks/recommended',
-		'plugin:@typescript-eslint/stylistic-type-checked',
 		'plugin:react/recommended',
 		'plugin:react/jsx-runtime',
+		'plugin:react-hooks/recommended',
+		'plugin:@typescript-eslint/strict-type-checked',
+		'plugin:@typescript-eslint/stylistic-type-checked',
+		'plugin:@tanstack/query/recommended',
 	],
 	ignorePatterns: ['dist', '.eslintrc.cjs'],
 	parser: '@typescript-eslint/parser',
@@ -22,5 +23,3 @@ const eslintConfig = {
 		tsconfigRootDir: __dirname,
 	},
 }
-
-export default eslintConfig
