47e0209c8e87bf637e134457cc7b9839bc2870d0
Panupong Tipjoi <itstoon.panupong@gmail.com>
2024-11-02 07:26:26 +0700

    chore: Removed unused pages

--- src/routes/about.lazy.tsx
+++
@@ -1,9 +0,0 @@
-import { createLazyFileRoute } from '@tanstack/react-router'
-
-export const Route = createLazyFileRoute('/about')({
-	component: AboutPage,
-})
-
-export function AboutPage() {
-	return <div>Hello /about!</div>
-}
