4f1a813275c370128d854008441d37bea68de0f4
Panupong Tipjoi <itstoon.panupong@gmail.com>
2024-11-09 19:29:51 +0700

    feat: Added money format

--- src/helpers/formatter.ts
+++ src/helpers/formatter.ts
@@ -2,3 +2,14 @@ export const DATE_FORMAT = 'YYYY-MM-DD'
 export const TIME_FORMAT = 'HH:mm'
 
 export const DATE_TIME_FORMAT = `${DATE_FORMAT} ${TIME_FORMAT}`
+
+export const moneyFormat = (value: number) => {
+	const formatter = new Intl.NumberFormat('th-TH', {
+		style: 'currency',
+		currency: 'THB',
+		minimumFractionDigits: 0,
+		maximumFractionDigits: 2,
+	})
+
+	return formatter.format(value)
+}
